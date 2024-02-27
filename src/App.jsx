0e47194ff9e9a63bc26364125ce3df8061bca526
callmecua1 <josuaoconner2321@gmail.com>
2024-02-27 13:09:07 +0700

    edit

--- src/App.jsx
+++ src/App.jsx
@@ -70,7 +70,7 @@ function App() {
   return (
     <>
        <div className="navbar">
-        <h2 className="navbar-title" onClick={()=>window.location.reload()}>Sushiku</h2>
+        <h2 className="navbar-title" onClick={()=>window.location.reload()}>Sashimi</h2>
         <img
           src={settings}
           id="open-tab-button"
--- src/components/pages-components/Footer.jsx
+++ src/components/pages-components/Footer.jsx
@@ -3,31 +3,31 @@ const Footer = () => {
     return (
       <footer className=" w-full relative flex mt-8 flex-col justify-center items-center">
         <div className="contact-icon flex justify-center items-center flex-col w-10/12 mt-8 pb-5 xl:w-2/4 xl:mt-10 ">
-          <h2 className="text-5xl xl:text-4xl mb-5 mt-5 font-bold cursor-pointer">Sushiku</h2>
+          <h2 className="text-5xl xl:text-4xl mb-5 mt-5 font-bold cursor-pointer">Sashimi</h2>
           <p className=" text-2xl xl:text-base mb-6">
             Everything we serve is made in house from the best ingredients.
           </p>
           <hr className="w-1/4 mb-10 h-1 rounded-full bg-red-500" />
           <div className="social-media w-full flex justify-center items-center">
             <img
-              src="src/assets/contact/email.png"
+              src="../contact/email.png"
               className=" w-12 h-12 md:w-14 md:h-14 xl:w-10 xl:h-10 "
               alt="feane"
             />
             <img
-              src="src/assets/contact/instagram.png"
+              src="../contact/instagram.png"
               className=" w-12 h-12 md:w-14 md:h-14 xl:w-10 xl:h-10 ml-8 mr-8"
               alt="feane"
             />
             <img
-              src="src/assets/contact/phone.png"
+              src="../contact/phone.png"
               className=" w-12 h-12 md:w-14 md:h-14 xl:w-10 xl:h-10"
               alt="feane"
             />
           </div>
         </div>
         <div className="footer-text-wrapper w-full h-16 xl:h-24 flex items-center justify-center bg-black mt-28 text-white">
-          <p className=" md:text-lg">© 2023 Nakamura Restaurant. All rights reserved</p>
+          <p className=" md:text-lg">© 2023 Sashimi Restaurant. All rights reserved</p>
         </div>
       </footer>
     );
